cc439ec8b0f65a5c1d58fd061569ac4275c18a36
Tpy6akyp87 <kaellakhim@gmail.com>
2024-06-26 12:52:23 +0300

    Value type and Reference type

---
+++ Learn/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    public class Employee : Person
+    {
+        public string EmployeeId { get; set; }
+        public string Contract { get; set; }
+        public int Salary { get; set; }
+    }
+    public class Client : Person
+    { 
+        public string ClientId { get; set; }
+        public string ClientEmail { get; set; }
+    }
+    public struct Currency
+    {
+        public string CurrencyCode { get; set; }
+        public string CurrencyName { get; set; }
+    }
+}
---
+++ Learn/PracticeWithTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using Models;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee employee = new Employee
+        {
+            FirstName = "Yodda",
+            LastName = "Bodda",
+            Age = 775,
+            EmployeeId = "R2D2",
+            Contract = "C3PO",
+            Salary = 5444
+        };        
+        Currency currency = new Currency
+        {
+            CurrencyCode = "USD",
+            CurrencyName = "Dollar"
+        };
+        UpdEmployeeContract(employee);
+        UpdCurrency(currency);
+    }
+    public static void UpdEmployeeContract(Employee employee)
+    {
+        employee.Contract = employee.Contract + "_upd";
+        Console.WriteLine($"Contract for {employee.FirstName} {employee.LastName} updated");
+        Console.WriteLine($"New contract is {employee.Contract}");
+    }
+    public static void UpdCurrency(Currency currency)
+    {
+        Console.WriteLine($"I will change {currency.CurrencyName} with code {currency.CurrencyCode}");
+        currency.CurrencyCode = "RMNR";
+        currency.CurrencyName = "Ruuble";
+        Console.WriteLine($"New currency is {currency.CurrencyName} with code {currency.CurrencyCode}");
+    }
+}
